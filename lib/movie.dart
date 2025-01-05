d9b1698bebf69a739a1fbb194803975a9c7d3d4e
leen12 <leensakka05@gmail.com>
2025-01-05 16:08:44 +0200

    commit

--- lib/movie.dart
+++ lib/movie.dart
@@ -2,7 +2,7 @@ class Movie {
   String name;
   int quantity;
   double price;
-  String category;  // Added category field
+  String category;
 
   Movie({required this.name, required this.quantity, required this.price, required this.category});
 }
